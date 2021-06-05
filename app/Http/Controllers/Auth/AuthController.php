6915c0ff464cd1768011e1d0a0d0504ccfca79b3
William Puerta <william@innotivamos.co>
2021-06-04 20:23:06 -0500

    Modificar validacion del lado del backend para login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -7,52 +7,62 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
 
     public function register(Request $request)
     {
-        $validateData = $request->validate([
+        $validateData = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8',
         ]);
 
+        if ($validateData->fails()) {
+            return response([
+                "errors" => $validateData->getMessageBag(),
+            ], 203);
+        }
+
+        $inputs = $request->all();
+
         $user = User::create([
-            'name' => $validateData['name'],
-            'email' => $validateData['email'],
-            'password' => Hash::make($validateData['password']),
+            'name' => $inputs['name'],
+            'email' => $inputs['email'],
+            'password' => Hash::make($inputs['password']),
         ]);
 
         $token = $user->createToken('auth_token')->plainTextToken;
 
         return response([
             'access_token' => $token,
-            'token_type' => 'Bearer'
+            'token_type' => 'Bearer',
+            'user' => $user
         ], 200);
     }
 
     public function login(Request $request)
     {
-        if(!Auth::attempt($request->only('email', 'password'))) {
+        if (!Auth::attempt($request->only('email', 'password'))) {
             return response([
-                'message'=> 'Credenciales inválidas'
+                'message' => 'Credenciales inválidas'
             ], 401);
         }
 
         $user = User::where('email', $request["email"])->first();
 
-        if(! $user){
+        if (!$user) {
             return response([
-                'message'=> 'No se ha encontrado al usuario'
+                'message' => 'No se ha encontrado al usuario'
             ], 404);
         }
 
         $token = $user->createToken('auth_token')->plainTextToken;
 
         return response([
-            'access_token'=>$token,
+            'access_token' => $token,
             'token_type' => 'Bearer'
         ]);
     }
